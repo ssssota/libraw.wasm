76c9ed545e49404b10b4b3dcdcd258519eba4dea
ssssota <tommysouta@gmail.com>
2024-10-28 22:51:38 +0900

    refactor: move files

--- www/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import "@builder.io/qwik/qwikloader.js";
-
-import { render } from "@builder.io/qwik";
-import "water.css/out/light.css";
-import { App } from "./app.tsx";
-import "./index.css";
-
-render(document.getElementById("app") as HTMLElement, <App />);
