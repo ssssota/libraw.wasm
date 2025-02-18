c927d82be6baaa1aa5a78929c3b3936bad69f445
ssssota <tommysouta@gmail.com>
2025-02-19 00:09:42 +0900

    chore: bumpup deps

--- playground/src/lib/resolvable.ts
+++ playground/src/lib/resolvable.ts
@@ -1,17 +1,17 @@
 export type ResolvablePromise<T> = Promise<T> & {
-  resolve(value: T): void;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  reject(reason: any): void;
+	resolve(value: T): void;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	reject(reason: any): void;
 };
 
 export function resolvable<T>(): ResolvablePromise<T> {
-  let resolve: (value: T) => void;
-  let reject: (reason: unknown) => void;
-  const promise = new Promise<T>((res, rej) => {
-    resolve = res;
-    reject = rej;
-  }) as ResolvablePromise<T>;
-  promise.resolve = resolve!;
-  promise.reject = reject!;
-  return promise;
+	let resolve: (value: T) => void;
+	let reject: (reason: unknown) => void;
+	const promise = new Promise<T>((res, rej) => {
+		resolve = res;
+		reject = rej;
+	}) as ResolvablePromise<T>;
+	promise.resolve = resolve!;
+	promise.reject = reject!;
+	return promise;
 }
