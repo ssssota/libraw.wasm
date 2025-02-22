a411eb9ddded64a21cdea13e30eee01502cf45c8
ssssota <tommysouta@gmail.com>
2025-02-23 01:30:39 +0900

    chore: fix lint warns

--- playground/src/lib/promise_with_resolvers.js
+++ playground/src/lib/promise_with_resolvers.js
@@ -3,7 +3,8 @@
  * @returns {{ promise: Promise<T>; resolve: (arg: T) => void; reject: (reason: any) => void }} - An object with a promise and its resolverss
  */
 export function promiseWithResolvers() {
-	let resolve, reject;
+	let resolve;
+	let reject;
 	const promise = new Promise((res, rej) => {
 		resolve = res;
 		reject = rej;
--- playground/src/lib/read_file.ts
+++ playground/src/lib/read_file.ts
@@ -1,20 +1,20 @@
-import { promiseWithResolvers } from './promise_with_resolvers';
+import { promiseWithResolvers } from "./promise_with_resolvers";
 
 export async function readFile(file: File): Promise<ArrayBuffer> {
 	const { promise, resolve, reject } = promiseWithResolvers<ArrayBuffer>();
 	const reader = new FileReader();
 	reader.addEventListener(
-		'load',
+		"load",
 		() => {
 			if (!(reader.result instanceof ArrayBuffer)) {
-				reject(new Error('Expected ArrayBuffer'));
+				reject(new Error("Expected ArrayBuffer"));
 				return;
 			}
 			resolve(reader.result);
 		},
-		{ once: true }
+		{ once: true },
 	);
-	reader.addEventListener('error', reject, { once: true });
+	reader.addEventListener("error", reject, { once: true });
 	reader.readAsArrayBuffer(file);
 	return promise;
 }
